d59507e1103eb595bd53412e3d2fad44ca58e02f
taxableapples <quizmastermike@gmail.com>
2023-09-08 13:06:28 -0700

    timer now based on system clock

--- TurnTimer.js
+++ TurnTimer.js
@@ -1,21 +1,19 @@
 var state = state || {};
 state.timer = state.timer || {};
 
-var timer_x = 100,
-timer_y = 100,
-fontSize = 150
+var timer_x = 101,
+timer_y = 101,
+fontSize = 150,
+TurnTimeLimit = 45,
+TimerFont = 'Crimson Text',
+TimerLayer = 'gmlayer'
 
 function createNewTimer(){
   var turnorder = Campaign().get('turnorder');
-  
   if (turnorder && turnorder !== "[]") {
-
     var turnorderArray = JSON.parse(turnorder);
-    
     if (turnorderArray.length > 0) {
-
       var activePlayerId = turnorderArray[0].id;
-
       var token = findObjs({
         _type: 'graphic',
         _subtype: 'token',
@@ -24,55 +22,59 @@ function createNewTimer(){
       })[0];
 
       if (token) {
+        var tokenLeft = token.get('left');
+        var tokenTop = token.get('top');
         var timerText = createObj('text', {
           pageid: Campaign().get('playerpageid'),
-          name: "Timer",
-          layer: 'gmlayer',
-          font_family: 'Tahoma',
-          top: timer_y,
-          left: timer_x,
+          layer: TimerLayer,
+          font_family: TimerFont,
+          top: tokenTop-100,
+          left: tokenLeft-100,
           width: timer_x,
           height: timer_y,
-          text: '30',
+          text: TurnTimeLimit.toString(),
           font_size: fontSize,
           color: '#FF0000',
         });
         
         state.timer[activePlayerId] = {
           textObjId: timerText.get('_id'),
-          time: 30,
-          timeout: null
+          time: TurnTimeLimit
         };
 
-        updateTimer(activePlayerId);
+        var startTime = new Date().getTime();
+
+        updateTimer(activePlayerId, startTime);
       }
     }
   }
 }
 
-function updateTimer(activePlayerId) {
-  try {
-    if (state.timer[activePlayerId].time >= 0) {
+function updateTimer(activePlayerId, startTime) {
+  var currentTime = Date.now();
+  var elapsedTime = (startTime - currentTime) / 1000;
+  var turnRemaining = Math.round(TurnTimeLimit + elapsedTime);
 
+  try {
+    if (turnRemaining >= 0) {
       var timerTextObj = getObj('text', state.timer[activePlayerId].textObjId);
       if (timerTextObj) {
         timerTextObj.set('text', state.timer[activePlayerId].time.toString());
       }
       
-      state.timer[activePlayerId].time--;
+      state.timer[activePlayerId].time = turnRemaining;
       setTimeout(function() {
-        updateTimer(activePlayerId);
+        updateTimer(activePlayerId, startTime);
       }, 1000);
     } else {
       var timerTextObj = getObj('text', state.timer[activePlayerId].textObjId);
       if (timerTextObj) {
         timerTextObj.remove();
       }
-      clearTimeout(state.timer[activePlayerId].timeout);
-      delete state.timer[activePlayerId];
     }
 
   } catch (error) {
+    log("error updating TurnTimer!")
     log(error)
   }
 }
@@ -80,9 +82,7 @@ function updateTimer(activePlayerId) {
 function removeOldTimer(){
   var oldtimer = findObjs({
     _type: 'text',
-    _pageid: Campaign().get("playerpageid"), 
-    top: timer_y,
-    left: timer_x,
+    _pageid: Campaign().get("playerpageid"),
     width: timer_x,
     height: timer_y
   })[0];
@@ -96,7 +96,7 @@ on('change:campaign:turnorder', function() {
 });
 
 on('chat:message', function(msg) {
-  if (msg.type === 'api' && msg.content.indexOf('!eot') === 0) {
+  if (msg.type === 'api' && msg.content.indexOf('!eot') === 0 || msg.type === 'api' && msg.content.indexOf('!pot') === 0) {
     removeOldTimer();
     createNewTimer();
   }
